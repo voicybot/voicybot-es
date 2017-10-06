2d75a9ed351a819dce032ad67e913fd3e1538dc3
Carl Kittelberger <icedream@icedream.pw>
2017-10-06 02:29:43 +0200

    Improve service logging for PulseAudio and X server.

--- packages/core/src/services/pulseaudio/index.js
+++ packages/core/src/services/pulseaudio/index.js
@@ -1,5 +1,6 @@
 import { resolve as resolvePath } from 'path';
 // import PulseAudio from 'pulseaudio';
+import StreamSplitter from 'stream-splitter';
 import Service from '../Service';
 
 export default class PulseAudioService extends Service {
@@ -38,10 +39,68 @@ export default class PulseAudioService extends Service {
   //   });
   // }
 
-  start() {
-    // HACK: calling original functions via super in async functions is incompatible with Babel < 7
-    return super.start('--start');
-    // return Service.prototype.start.apply(this, '--start');
+  forwardLog(_token) {
+    const { logger } = this;
+
+    const token = _token.trim(); // get rid of \r
+    const level = token.substring(0, 1).toUpperCase();
+    const msg = token.substring(token.indexOf(']') + 2);
+    switch (level) {
+      case 'D': return logger.silly(msg);
+      case 'I': return logger.silly(msg);
+      case 'W': return logger.warn(msg);
+      case 'E': return logger.error(msg);
+      default: return logger.silly(msg);
+    }
+  }
+
+  getProcessOptions() {
+    return {
+      ...super.getProcessOptions(),
+      stdio: ['ignore', 'pipe', 'pipe'],
+      detached: false,
+    };
+  }
+
+  async start() {
+    // HACK - can't use await here because of a bug in babel handling super in async functions
+    await Service.prototype.start.apply(this);
+
+    return new Promise((resolve, reject) => {
+      const service = this;
+
+      const proc = this.subprocess;
+
+      let calledCallback = false;
+
+      // logging
+      const tokenHandler = (token) => {
+        service.forwardLog(token);
+
+        if (!calledCallback && (
+          /* (token.indexOf('client.conf') >= 0) || */
+          (token.indexOf('Daemon startup complete.') >= 0))) {
+          calledCallback = true;
+          this.process = proc;
+          setTimeout(resolve, 1500); // TODO: Use some more stable condition
+        }
+      };
+
+      const stdoutTokenizer = proc.stdout.pipe(StreamSplitter('\n'));
+      stdoutTokenizer.encoding = 'utf8';
+      stdoutTokenizer.on('token', tokenHandler);
+
+      const stderrTokenizer = proc.stderr.pipe(StreamSplitter('\n'));
+      stderrTokenizer.encoding = 'utf8';
+      stderrTokenizer.on('token', tokenHandler);
+
+      proc.once('exit', () => {
+        if (!calledCallback) {
+          calledCallback = true;
+          reject(new Error('PulseAudio daemon terminated unexpectedly.'));
+        }
+      });
+    });
   }
 }
 
--- packages/core/src/services/xorg/index.js
+++ packages/core/src/services/xorg/index.js
@@ -1,5 +1,8 @@
 import { resolve as resolvePath } from 'path';
+import StreamSplitter from 'stream-splitter';
 import Service from '../Service';
+
+const xorgLogMatcher = /^\((--|\*\*|==|\+\+|!!|II|WW|EE|NI|\?\?)\) (.+)$/;
 
 export default class XorgService extends Service {
   constructor({
@@ -26,11 +29,96 @@ export default class XorgService extends Service {
     });
   }
 
+  forwardLog(_token) {
+    const { logger } = this;
+
+    const token = _token.trim(); // get rid of \r
+
+    const logMatch = xorgLogMatcher.exec(token);
+    if (logMatch) {
+      switch (logMatch[1]) {
+        // case "--": // probed
+        // case "**": // from config file
+        // case "==": // default setting
+        // case "++": // from command line
+        case '!!': // notice
+          logger.notice(logMatch[2]);
+          break;
+        case 'II': // info
+          logger.info(logMatch[2]);
+          break;
+        case 'WW': // warn
+        case 'NI': // not implemented
+          logger.warn(logMatch[2]);
+          break;
+        case 'EE': // error
+          logger.error(logMatch[2]);
+          break;
+          // case "??": // Unknown
+        default:
+          logger.debug(logMatch[2]);
+          break;
+      }
+      return;
+    }
+
+    logger.debug(token);
+  }
+
+  getProcessOptions() {
+    return {
+      ...super.getProcessOptions(),
+      stdio: ['ignore', 'pipe', 'pipe'],
+      detached: false,
+    };
+  }
+
   getProcessArguments(...args) {
     return super.getProcessArguments(
       ...this.extensions.reduce((arr, cur) => [...arr, '+extension', cur], []),
       ...args,
       this.display,
     );
+  }
+
+  async start() {
+    // HACK - can't use await here because of a bug in babel handling super in async functions
+    await Service.prototype.start.apply(this);
+
+    return new Promise((resolve, reject) => {
+      const service = this;
+
+      const proc = this.subprocess;
+
+      let calledCallback = false;
+
+      // logging
+      const tokenHandler = (token) => {
+        service.forwardLog(token);
+
+        if (!calledCallback && (
+          /* (token.indexOf('client.conf') >= 0) || */
+          (token.indexOf('Using system config directory') >= 0))) {
+          calledCallback = true;
+          this.process = proc;
+          setTimeout(resolve, 1500); // TODO: Use some more stable condition
+        }
+      };
+
+      const stdoutTokenizer = proc.stdout.pipe(StreamSplitter('\n'));
+      stdoutTokenizer.encoding = 'utf8';
+      stdoutTokenizer.on('token', tokenHandler);
+
+      const stderrTokenizer = proc.stderr.pipe(StreamSplitter('\n'));
+      stderrTokenizer.encoding = 'utf8';
+      stderrTokenizer.on('token', tokenHandler);
+
+      proc.once('exit', () => {
+        if (!calledCallback) {
+          calledCallback = true;
+          reject(new Error('X server terminated unexpectedly.'));
+        }
+      });
+    });
   }
 }
